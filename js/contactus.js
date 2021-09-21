acfe0c6dd184a8ecfa80edd27c6fc35d8dc97ba2
echwaadams <adamsechwa18@gmail.com>
2021-09-21 23:01:36 +0300

    add alert to inform user after filling the form

--- js/contactus.js
+++ js/contactus.js
@@ -6,5 +6,10 @@ $(document).ready(function (){
         const name = $('input#name').val();
         const email = $('input#email').val();
         const message = $('textarea#message').val();
+
+        // popping a message to alert the user after filling the form
+        if($("input#name").val() && $("input#email").val()) {
+            alert(' Hi ' + name + 'We have received your message. Thank you for reaching out to us.')
+        }
     })
 })
